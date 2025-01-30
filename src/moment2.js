8bb101cad81cbe87ff637b00b19bc7c5c8f4008c
Amanda Persdotter <amer1301@student.miun.se>
2025-01-30 14:53:14 +0100

    innan sort

--- src/moment2.js
+++ src/moment2.js
@@ -8,7 +8,40 @@ async function fetchData() {
 // Asynkron Funktion som inväntar och visar data 
 async function displayData() { 
     let data = await fetchData(); 
-    console.log(data); 
+
+const tableBody = document.querySelector("#courseTable tbody");
+
+//Rensa tabellen om det redan finns data
+tableBody.innerHTML = "";
+
+data.forEach(course => {
+    const row = document.createElement('tr'); //skapar ny rad
+
+    //skapar celler och lägger till dem
+    const codeCell = document.createElement('td');
+    codeCell.textContent = course.code;
+    row.appendChild(codeCell);
+
+
+    const nameCell = document.createElement('td');
+    nameCell.textContent = course.coursename;
+    row.appendChild(nameCell);
+
+    const progressionCell = document.createElement('td');
+    progressionCell.textContent = course.progression;
+    row.appendChild(progressionCell);
+
+    //lägger till kursplan som en länk
+    const syllabusCell = document.createElement('td');
+    const syllabusLink = document.createElement('a');
+    syllabusLink.href = course.syllabus;
+    syllabusLink.textContent = "Kursplan";
+    syllabusLink.target = "_blank"; //Öppnar länken i ny flik
+    syllabusCell.appendChild(syllabusLink);
+    row.appendChild(syllabusCell);
+
+    tableBody.appendChild(row);
+});
 } 
  
 displayData(); 
