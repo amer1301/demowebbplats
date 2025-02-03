c66c38dd5a7dc5b953bbc2b9fce808a462601d64
Amanda Persdotter <amer1301@student.miun.se>
2025-02-03 16:24:53 +0100

    tagit bort filer

--- dist/index.cde3bbbf.js
+++
@@ -1,2 +0,0 @@
-const e=["slide1","slide2","slide3","slide4","slide5"];let t=0;function s(t){e.forEach(e=>{document.getElementById(e).classList.remove("active")}),document.getElementById(e[t]).classList.add("active")}setInterval(function(){s(t=(t+1)%e.length)},3e3),s(t);
-//# sourceMappingURL=index.cde3bbbf.js.map
